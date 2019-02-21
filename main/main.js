8d4bc236cebcec0922a5b7b2ba1db5a3be39b954
jingliang0414 <jingliang0414@hotmail.com>
2019-02-21 15:22:35 +0800

    TWS-PRACTICE-Week3

--- main/main.js
+++ main/main.js
@@ -1,11 +1,18 @@
-function main(kilometers, ...waitingTime) {
-    let pricePerKilo = 0.8,
+function main(inputs) {
+    if (inputs === null || inputs === '') {
+        return "请输入行驶公里数与等待时间，期待输入为“行驶公里数 等待时间”（等待时间可选））";
+    }
+
+    let inputsList = inputs.split(' ');
+
+    let kilometers = parseFloat(inputsList[0]),
+        pricePerKilo = 0.8,
         waitingPricePerKilo = 0.25,
         result = 0,
         reg = /^\d+(\.{0,1}\d*)/;
 
     if (!reg.test(kilometers)) {
-        return "输入不符合要求，期待输入为“行驶公里数 等待时间”（等待时间可选））";
+        return "行驶公里数输入不符合要求，期待输入为“行驶公里数 等待时间”（等待时间可选））";
     }
 
     if (kilometers <= 2) {
@@ -16,9 +23,10 @@ function main(kilometers, ...waitingTime) {
         result = 6 + (kilometers - 2) * pricePerKilo * 1.5
     }
 
-    if (waitingTime.length != 0) {
+    if (inputsList.length > 1) {
+        let waitingTime = parseFloat(inputsList[1]);
         if (!reg.test(waitingTime)) {
-            return "输入不符合要求，期待输入为“行驶公里数 等待时间”（等待时间可选））";
+            return "等待时间输入不符合要求，期待输入为“行驶公里数 等待时间”（等待时间可选））";
         } else {
             result += waitingPricePerKilo * waitingTime
         }
@@ -26,4 +34,8 @@ function main(kilometers, ...waitingTime) {
 
     return Math.round(result);
 };
+
+function buildInputs() {
+
+}
 module.exports = main;
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -4,64 +4,60 @@ describe('taxi fee', function() {
     it("should give error information when input is in a wrong format", function() {
         let result1 = main(null);
         let result2 = main("");
-        let result3 = main(-9);
-        let result4 = main("7");
-        let result5 = main(1, null);
-        let result6 = main(1, "");
-        let result7 = main(1, -2);
-        let result8 = main(1, "7");
-        expect(result1).toString("输入不符合要求，期待输入为“行驶公里数 等待时间”（等待时间可选））")
-        expect(result2).toString("输入不符合要求，期待输入为“行驶公里数 等待时间”（等待时间可选））")
-        expect(result3).toString("输入不符合要求，期待输入为“行驶公里数 等待时间”（等待时间可选））")
-        expect(result4).toString("输入不符合要求，期待输入为“行驶公里数 等待时间”（等待时间可选））")
-        expect(result5).toString("输入不符合要求，期待输入为“行驶公里数 等待时间”（等待时间可选））")
-        expect(result6).toString("输入不符合要求，期待输入为“行驶公里数 等待时间”（等待时间可选））")
-        expect(result8).toString("输入不符合要求，期待输入为“行驶公里数 等待时间”（等待时间可选））")
+        let result3 = main("-9");
+        let result4 = main("abc");
+        let result5 = main("1 -2");
+        let result6 = main("1 s");
+        expect(result1).toString("请输入行驶公里数与等待时间，期待输入为“行驶公里数 等待时间”（等待时间可选））")
+        expect(result2).toString("请输入行驶公里数与等待时间，期待输入为“行驶公里数 等待时间”（等待时间可选））")
+        expect(result3).toString("行驶公里数输入不符合要求，期待输入为“行驶公里数 等待时间”（等待时间可选））")
+        expect(result4).toString("行驶公里数输入不符合要求，期待输入为“行驶公里数 等待时间”（等待时间可选））")
+        expect(result5).toString("等待时间输入不符合要求，期待输入为“行驶公里数 等待时间”（等待时间可选））")
+        expect(result6).toString("等待时间输入不符合要求，期待输入为“行驶公里数 等待时间”（等待时间可选））")
     })
 
     it("should calculate total price when distance is less than 2 kilometers and waiting time is 0", function() {
-        let kilometers = 1;
-        let result = main(kilometers);
+        let inputs = "1";
+        let result = main(inputs);
         expect(result).toEqual(6);
     });
 
     it("should calculate total price when distance is greater than 2 kilometers but less than 8 kilometers, and waiting time is 0", function() {
-        let kilometers = 4;
-        let result = main(kilometers);
+        let inputs = "4";
+        let result = main(inputs);
         expect(result).toEqual(8);
     });
 
     it("should calculate total price when distance is greater than 8 kilometers and waiting time is 0", function() {
+        let inputs = "8";
         let kilometers = 8;
-        let result = main(kilometers);
+        let result = main(inputs);
         expect(result).toEqual(13);
     });
 
     it("should calculate total price when distance is less than 2 kilometers, and has waiting time", function() {
-        let kilometers = 1,
-            waitingTime = 4;
-        let result = main(kilometers, waitingTime);
+        let inputs = "1 4";
+        let result = main(inputs);
         expect(result).toEqual(7);
     });
 
     it("should calculate integer total price when distance is less than 2 kilometers, and has waiting time and the total price is a decimal number", function() {
-        let kilometers = 0.5,
-            waitingTime = 4.6;
-        let result = main(kilometers, waitingTime);
+        let inputs = "0.5 4.6";
+
+        let result = main(inputs);
         expect(result).toEqual(7);
     });
 
     it("should calculate total price when distance is greater than 2 kilometers but less than 8 kilometers, and has waiting time", function() {
-        let kilometers = 4,
-            waitingTime = 4;
-        let result = main(kilometers, waitingTime);
+        let inputs = "4 4";
+
+        let result = main(inputs);
         expect(result).toEqual(9);
     });
 
     it("should calculate total price when distance is greater than 8 kilometers, and has waiting time", function() {
-        let kilometers = 8,
-            waitingTime = 4;
-        let result = main(kilometers, waitingTime);
+        let inputs = "8 4";
+        let result = main(inputs);
         expect(result).toEqual(14);
     });
 
